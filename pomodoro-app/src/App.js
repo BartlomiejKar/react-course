a8a0f3d22728ac7cfbfced2fbb8d4e8834806bbc
Bartek Karelus <bartek.karelus@gmail.com>
2019-09-13 16:03:18 +0200

    import styles

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TimeboxList, EditorTimeable } from "./Components"
+
 function App() {
     return (
         <div className="App">
--- pomodoro-app/src/index.js
+++ pomodoro-app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "../src/styles/style.css";
+import App from "../src/App";
 
-import App from "../src/App"
 
 
 
