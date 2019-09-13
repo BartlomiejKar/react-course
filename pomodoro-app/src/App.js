c30b97fc35af0503f718450dd31fc8091e5540de
Bartek Karelus <bartek.karelus@gmail.com>
2019-09-13 15:57:56 +0200

    extract App components

---
+++ pomodoro-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TimeboxList, EditorTimeable } from "./Components"
+function App() {
+    return (
+        <div className="App">
+            <TimeboxList />
+            <EditorTimeable />
+        </div>
+    );
+}
+export default App;
---
+++ pomodoro-app/src/Clock.js
@@ -0,0 +1,41 @@
+import React from 'react';
+function Clock({
+    className = "",
+    hours = 0,
+    minutes = 0,
+    seconds = 0,
+    miliseconds = 0
+}) {
+    return (
+        <h2 className={"Clock " + className}>
+            Pozostało {Hours(hours)}:{Minutes(minutes)}:{Seconds(seconds)}:
+{Miliseconds(miliseconds)}
+        </h2>
+    );
+}
+function Hours(hours) {
+    const TimeHours = hours < 0 ? `00` : hours < 10 ? `0${hours}` : hours;
+    return `${TimeHours}`;
+}
+
+function Minutes(minutes) {
+    const TimeMinutes =
+        minutes < 0 ? `00` : minutes < 10 ? `0${minutes}` : minutes;
+    return `${TimeMinutes}`;
+}
+function Seconds(seconds) {
+    const TimeSeconds =
+        seconds < 0 ? `00` : seconds < 10 ? `0${seconds}` : seconds;
+    return `${TimeSeconds}`;
+}
+function Miliseconds(miliseconds) {
+    const TimeMiliseconds =
+        miliseconds < 0
+            ? `00`
+            : miliseconds < 10
+                ? `0${miliseconds}`
+                : miliseconds;
+    return `${TimeMiliseconds}`;
+}
+
+export default Clock;
