a4dd92ca0256077e930cc477435970524b21fea0
Bartek Karelus <bartek.karelus@gmail.com>
2019-09-18 18:06:03 +0200

    add form with className

--- pomodoro-app/src/Timebox/Timebox.js
+++ pomodoro-app/src/Timebox/Timebox.js
@@ -2,8 +2,20 @@ import React from 'react';
 import "./Timebox.css"
 
 class Timebox extends React.Component {
-
-
+    state = {
+        title: "",
+        totalTimes: ""
+    }
+    handleChangeTitle = (e) => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+    handleChangeTimes = (e) => {
+        this.setState({
+            totalTimes: e.target.value
+        })
+    }
     render() {
         const { title, totalTimes, onEdit, onDelete, index, confirmChanges, disabledButton } = this.props
         return (
@@ -14,6 +26,14 @@ class Timebox extends React.Component {
                 <button onClick={onEdit}>Edytuj</button>
                 <button onClick={onDelete}>Usun</button>
                 <button className={disabledButton ? "" : "Timebox__buttonConfirm"} disabled={!disabledButton} onClick={confirmChanges}>Zatwierdź zmiany</button>
+                <form className={disabledButton ? "" : "Timebox__formEdit"}>
+                    <label> Co robisz
+                        <input defaultValue={this.state.title} onChange={this.state.handleChangeTitle} type="text" />
+                    </label>
+                    <label> ile minut
+                        <input defaultValue={this.state.totalTimes} onChange={this.state.handleChangeTimes} type="number" />
+                    </label>
+                </form>
             </div>
         );
     }
