44552b0c5c98ce9235ba145305aef3c5b93389bd
Bartek Karelus <bartek.karelus@gmail.com>
2019-09-24 19:43:52 +0200

    added ErrorBoundaries

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import TimeboxList from "./TimeboxList/TimeboxList";
 import EditorTimeable from "./EditorTimeable/EditorTimeable"
 import "../src/styles/style.css"
+import ErrorBoundaries from "../src/ErrorBoundaries/ErrorBoundaries"
 
 function App() {
     return (
         <div className="App">
-            <React.StrictMode>
-                <TimeboxList />
-                <EditorTimeable />
-            </React.StrictMode>
+            <ErrorBoundaries message="Nie działa cała aplikacja">
+                <React.StrictMode>
+                    <TimeboxList />
+                    <EditorTimeable />
+                </React.StrictMode>
+            </ErrorBoundaries>
         </div>
     );
 }
