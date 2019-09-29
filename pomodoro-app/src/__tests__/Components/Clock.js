fc4f581fc262a23f1b06b4bdd90080566ba8263b
Bartek Karelus <bartek.karelus@gmail.com>
2019-09-29 13:18:20 +0200

    use test renderer and snapshot

--- pomodoro-app/src/__tests__/Components/Clock.js
+++ pomodoro-app/src/__tests__/Components/Clock.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Clock from "../../Clock/Clock"
 import ReactDOM from "react-dom"
-
+import renderer from "react-test-renderer"
+var clockRenderer = null;
 var root = null
 describe("<Clock/>", () => {
-    describe("when gives minutes and seconds", () => {
+    describe("when gives minutes and seconds (DOM)", () => {
         beforeEach(() => {
             root = document.createElement("div");
             ReactDOM.render(
@@ -25,5 +26,26 @@ describe("<Clock/>", () => {
     })
     it("render time properly", () => {
         expect(root.childNodes[0].textContent).toMatch(/20:10/)
+    })
+
+    describe("when gives minutes and seconds(react-test-renderer)", () => {
+        beforeEach(() => {
+            clockRenderer = renderer.create(
+                <Clock minutes={20} seconds={10} />
+            );
+        })
+        it("renders properly", () => {
+            console.log(clockRenderer.toJSON())
+            expect(clockRenderer.toJSON()).toMatchSnapshot()
+        })
+    })
+    it("render h2 element", () => {
+        expect(clockRenderer.toJSON().type).toEqual("h2")
+    })
+    it("set className", () => {
+        expect(clockRenderer.toJSON().props).toMatchObject({ "className": expect.stringMatching(/Clock/) })
+    })
+    it("render time properly", () => {
+        expect(clockRenderer.toJSON().children).toEqual(expect.arrayContaining(["20", "10"]))
     })
 })
