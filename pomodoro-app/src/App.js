ccd9cd36df78c8d0dba646b895688f14de1f2599
Bartek Karelus <bartek.karelus@gmail.com>
2019-10-21 19:03:04 +0200

    create header in App and App.css

--- pomodoro-app/src/App.js
+++ pomodoro-app/src/App.js
@@ -3,11 +3,16 @@ import TimeboxList from "./TimeboxList/TimeboxList";
 import EditorTimeable from "./EditorTimeable/EditorTimeable"
 import "../src/styles/style.css"
 import ErrorBoundaries from "../src/ErrorBoundaries/ErrorBoundaries"
+import "./App.css"
 
 function App() {
     return (
         <div className="App">
             <ErrorBoundaries message="Nie działa cała aplikacja">
+                <header className="Header__email">
+                    Witaj example@example.pl
+            <a className="Header__email-link" href="/#">wyloguj się</a>
+                </header>
                 <React.StrictMode>
                     <TimeboxList />
                     <EditorTimeable />
